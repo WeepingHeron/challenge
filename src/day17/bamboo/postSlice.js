d1e0432bd92576b0990ffd6036e21f4e3f9f83ed
WeepingHeron <kimtaehoon_98@naver.com>
2023-04-03 21:53:03 +0900

    day17

---
+++ src/day17/bamboo/postSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const getPostsThunk = createAsyncThunk('getPosts', async (payload, thunk) => {
+  const data = await axios.get('http://localhost:3001/posts')
+  console.log(data.data)
+  return thunk.fulfillWithValue(data.data)
+})
+
+const initialState = {
+  posts: [],
+  post: {},
+}
+
+const postSlice = createSlice({
+  name: 'postSlice',
+  initialState,
+  reducers: {
+    addPost: (state, action) => {
+      state.posts = [...state.posts, action.payload]
+    },
+
+    deletePost: (state, action) => {
+      state.posts = state.posts.filter((post) => post.id !== action.payload)
+    },
+
+    editPost: (state, action) => {
+      const { id, title, content } = action.payload
+      const existingPost = state.posts.find((post) => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+
+    getPostByID: (state, action) => {
+      const postId = action.payload
+      state.post = state.posts.find((post) => post.id === postId)
+    },
+  },
+  extraReducers: {
+    [getPostsThunk.fulfilled]: (state, action) => {
+      return { ...state, posts: action.payload }
+    },
+  },
+})
+
+export default postSlice
+export const { addPost, deletePost, editPost, getPostByID } = postSlice.actions
--- src/example/bamboo/postSlice.js
+++ src/example/bamboo/postSlice.js
@@ -1,59 +1,57 @@
 // Redux requires that we write all state updates immutably, by making copies of data and updating the copies.
 // However, RTK's createSlice and createReducer APIs use Immer inside to allow us to write 'muatating' update logic that becomes correct immutable updates.
 
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios"
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-export const getPostsThunk = createAsyncThunk('getPosts', async(payload, thunk) => {
-    //서버에서 가져오는 로직
-    const data = await axios.get('http://localhost:3001/posts')
-    console.log(data.data)
-    // 서버에서 가져온 posts를 store에 넣기
-    return thunk.fulfillWithValue(data.data)
+export const getPostsThunk = createAsyncThunk('getPosts', async (payload, thunk) => {
+  //서버에서 가져오는 로직
+  const data = await axios.get('http://localhost:3001/posts')
+  console.log(data.data)
+  // 서버에서 가져온 posts를 store에 넣기
+  return thunk.fulfillWithValue(data.data)
 })
 
 const initialState = {
-    posts: [], post:{}
-};
+  posts: [],
+  post: {},
+}
 
 const postSlice = createSlice({
-    name: 'postSlice',
-    initialState,
-    reducers: {
-        addPost: (state, action) => {
-            state.posts = [...state.posts, action.payload]
-        },
-
-        deletePost: (state, action) => {
-            state.posts = state.posts.filter((post) => post.id !== action.payload)
-        },
-
-        editPost: (state, action) => {
-            const { id, title, content } = action.payload;
-            const existingPost = state.posts.find((post) => post.id === id);
-            if (existingPost) {
-                existingPost.title = title;
-                existingPost.content = content;
-            }
-        },
-
-        getPostByID: (state, action) => {
-            const postId = action.payload;
-            state.post = state.posts.find((post) => post.id === postId);
-          },
-          
-        
+  name: 'postSlice',
+  initialState,
+  reducers: {
+    addPost: (state, action) => {
+      state.posts = [...state.posts, action.payload]
     },
-    extraReducers: {
-        [getPostsThunk.fulfilled]: (state, action) => {
-          return { ...state, posts: action.payload };
-        }
-      }
-      
-    });
 
-export default postSlice;
-export const { addPost, deletePost, editPost, getPostByID } = postSlice.actions;
+    deletePost: (state, action) => {
+      state.posts = state.posts.filter((post) => post.id !== action.payload)
+    },
+
+    editPost: (state, action) => {
+      const { id, title, content } = action.payload
+      const existingPost = state.posts.find((post) => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+
+    getPostByID: (state, action) => {
+      const postId = action.payload
+      state.post = state.posts.find((post) => post.id === postId)
+    },
+  },
+  extraReducers: {
+    [getPostsThunk.fulfilled]: (state, action) => {
+      return { ...state, posts: action.payload }
+    },
+  },
+})
+
+export default postSlice
+export const { addPost, deletePost, editPost, getPostByID } = postSlice.actions
 
 // 1. 화면에 addPost로 해서 내용 띄우기
 
